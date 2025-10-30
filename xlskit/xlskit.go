48c365d04390860f9cb4808e85cc02779c5e6e45
turingdance <271151388@qq.com>
2025-10-30 22:23:56 +0800

    A

--- xlskit/xlskit.go
+++ xlskit/xlskit.go
@@ -19,13 +19,14 @@ func NewXlsCtrl(sheetname string) *xlskitctrl {
 	}
 }
 
+func Render(title string, meta []Meta, dataInput []map[string]any) (buf *bytes.Buffer, err error) {
+	ctrl := NewXlsCtrl(title)
+	return ctrl.Render(meta, dataInput)
+}
+
 // meta: 是元数据,{"字段1","字段2","字符3"}
-func (s *xlskitctrl) Render(meta []Meta, dataInput []map[string]any) (buf *bytes.Buffer, err error) {
-	index, err := s.file.NewSheet(s.sheetname)
-	if err != nil {
-		return
-	}
-	s.file.SetActiveSheet(index)
+func BuildData(meta []Meta, dataInput []map[string]any) (list [][]CellValue) {
+
 	titleMap := make(map[string]string, 0)
 	// 形成字段名称和A,B,C,D之间的映射关系
 	fieldArr := []string{}
@@ -35,17 +36,44 @@ func (s *xlskitctrl) Render(meta []Meta, dataInput []map[string]any) (buf *bytes
 	}
 	// 添加表头
 	rowIndex := 1
+	list = make([][]CellValue, 0)
+	titlerow := make([]CellValue, 0)
 	for index, field := range fieldArr {
 		cellName := fmt.Sprintf("%s%d", charfileds[index], rowIndex)
 		title := titleMap[field]
-		s.file.SetCellValue(s.sheetname, cellName, title)
+		titlerow = append(titlerow, CellValue{
+			Name:  cellName,
+			Value: title,
+		})
 	}
+	list = append(list, titlerow)
+
 	for rowIndex, dataItem := range dataInput {
+		datarow := make([]CellValue, 0)
 		for i, field := range fieldArr {
 			cellName := fmt.Sprintf("%s%d", charfileds[i], rowIndex+2)
 			value := dataItem[field]
-			s.file.SetCellValue(s.sheetname, cellName, value)
-			//fmt.Println(cellName, field, titleMap[field], value)
+			datarow = append(datarow, CellValue{
+				Name:  cellName,
+				Value: value,
+			})
+		}
+		list = append(list, datarow)
+	}
+	return list
+}
+
+// meta: 是元数据,{"字段1","字段2","字符3"}
+func (s *xlskitctrl) Render(meta []Meta, dataInput []map[string]any) (buf *bytes.Buffer, err error) {
+	index, err := s.file.NewSheet(s.sheetname)
+	if err != nil {
+		return
+	}
+	s.file.SetActiveSheet(index)
+	data := BuildData(meta, dataInput)
+	for _, row := range data {
+		for _, c := range row {
+			s.file.SetCellValue(s.sheetname, c.Name, c.Value)
 		}
 	}
 	return s.file.WriteToBuffer()
